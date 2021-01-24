42420f04b8672355f43e29fec064ae024d313fb9
openxw <openxw@qq.com>
2021-01-24 15:43:19 +0800

    修复 N+1 问题

---
+++ app/Models/Topic.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        # code...
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        # code...
+        return $this->belongsTo(User::class);
+    }
+}
