8713b7b0cb3bfa6136d04a2269b5b854e17668c3
openxw <openxw@qq.com>
2021-01-25 20:38:13 +0800

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,4 +33,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        # code...
+        return $this->id == $model->user_id;
+    }
 }
