bb613dae9a14f85e5405698a61ee0755e0398f47
openxw <openxw@qq.com>
2021-01-26 10:58:00 +0800

    用户可以发表评论

--- database/seeders/RepliesTableSeeder.php
+++ database/seeders/RepliesTableSeeder.php
@@ -1,14 +1,35 @@
 <?php
 
-namespace  Database\Seeders;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Seeder;
 use App\Models\Reply;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ReplyRequest;
+use Auth;
 
-class RepliesTableSeeder extends Seeder
+class RepliesController extends Controller
 {
-    public function run()
+    public function __construct()
     {
-        Reply::factory()->times(1000)->create();
+        $this->middleware('auth');
+    }
+
+    public function store(ReplyRequest $request, Reply $reply)
+    {
+        $reply->content = $request->content;
+        $reply->user_id = Auth::id();
+        $reply->topic_id = $request->topic_id;
+        $reply->save();
+
+        return redirect()->to($reply->topic->link())->with('success', '评论创建成功！');
+    }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return redirect()->route('replies.index')->with('success', '评论删除成功！');
     }
 }
